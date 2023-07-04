6b7619ea21c4db7f5fa56aeb4f989ca4a65a6d10
Arthur Ferraz <arthursnc@gmail.com>
2023-07-04 20:23:34 -0300

    [docs] Adiciona conteúdo ao readme

--- lambda.cpp
+++ lambda.cpp
@@ -2,25 +2,27 @@
 #include<vector>
 #include<math.h>
 
+/*
+Esse código tem objetivo de demonstrar a prototipagem de funções,
+ A definição de funções lambda e sua utilização, em contraste com 
+ as funções tradicionais.
+*/
 using namespace std;
 
+//aqui a função absolut é prototipada
 double absolut(int a);
+
+//função tradicional
 int conventional()
 {
     int a = -4;
     double b = absolut(a);
- /*
-    double absolut(int a)
-    {   
-        // não funciona definição 
-        //de funções tradicionais 
-        //dentro do escopo de outras funções
-        return math.sqrt(double(a)*double(a));
-    }
-*/
+    //Utilizando a função prototipada absolut
     cout<<"módulo de "<<a<<" é "<< absolut(a) <<endl;
     return 0;
 }
+
+//utiliza função lambda 
 int lambdaCapture()
 {
     int a = -4;
@@ -34,6 +36,7 @@ int lambdaCapture()
     cout<<"módulo de "<<a<<" é "<< func()<<endl;
     return 0;
 }
+
 int main()
 {
     conventional();
@@ -41,6 +44,7 @@ int main()
     return 0;
 }
 
+//aqui a função absolut é definida
 double absolut(int a)
 {
     return sqrt((double)a*(double)a);
--- pointerRef.cpp
+++ pointerRef.cpp
@@ -3,27 +3,41 @@
 #include<math.h>
 
 using namespace std;
-
+/*
+Esse código tem objetivo de demonstrar a definição de funções lambda 
+ com valores de retorno e utilizando parâmetros.
+Também ilustra o uso de ponteiros vs referências quando parâmetros
+ de funções.
+*/
 int lambdaCapture()
 {
     int a = -4;
     int b = -4;
+    //declarando ponteiro vazio
     int* ptra = NULL;
+    //definindo referência para ponteiro b
     int& refb = b;
+    //atribuindo endereço de a ao ponteiro
     ptra = &a;
     
+    //função lambda que retorna o módulo de um número, 
+    //recebendo um ponteiro como parâmetro
     auto funcptr = [](int* ptra) -> double
     {
         return sqrt(*ptra * *ptra);
     };
+    //função lambda que retorna o módulo de um número
+    //recebendo uma referência como parâmetro
     auto funcref = [](int& refb) -> double
     {
         return sqrt(refb * refb);
     };
+    
     cout<<"módulo de "<<a<<" é "<< funcptr(ptra)<<endl;
     cout<<"módulo de "<<b<<" é "<< funcref(refb)<<endl;
     return 0;
 }
+
 int main()
 {
     lambdaCapture();
